ca3564f152b6a9606f21333b12274cf83687f710
Welyab Paula <welyab.exe@gmail.com>
2020-08-24 17:14:08 -0300

    implementacao h2

--- src/main/java/com/github/moisesnascimento/universidade/UniversidadeApplication.java
+++ src/main/java/com/github/moisesnascimento/universidade/UniversidadeApplication.java
@@ -3,12 +3,14 @@ package com.github.moisesnascimento.universidade;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {
 		// aqui estamos dizendo para o spring buscar os componentes nesse pacote
 		"com.github.moisesnascimento.universidade.application",
 })
+@Profile("h2")
 public class UniversidadeApplication {
 
 	public static void main(String[] args) {
